8a2b265d9ca1eb3a91232b01d32b1badfb3dd5f2
Darren Cato <darren@cxalloy.com>
2012-09-11 02:34:15 -0400

    project start, may expand it on it or just use for reference

---
+++ nodeChat.js
@@ -0,0 +1,47 @@
+var net = require('net');
+var chatServer = net.createServer(),
+	clientList = [];
+
+chatServer.on('connection', function(client){
+
+	// get client details and say hello
+	client.name = client.remoteAddress + ':' + client.remotePort;
+	client.write('Hi ' + client.name + '!\n');
+	console.log(client.name + ' joined');
+	clientList.push(client);
+
+	// on event
+	client.on('data', function(data) {
+		broadcast(data, client);
+	});
+
+	client.on('end', function() {
+		console.log(client.name + ' quit');
+		clientList.splice(clientList.indexOf(client), 1);
+	});
+
+	client.on('error', function(e) {
+		console.log(e);
+	});
+});
+
+function broadcast(message, client){
+	var cleanup = [];
+	for(var i=0;i<clientList.length;i+=1){
+		if(client !== clientList[i]){
+			if(clientList[i].writable) {
+				clientList[i].write(client.name + " says " + message);
+			} else {
+				cleanup.push(clientList[i]);
+				clientList[i].destroy();
+			}
+		}
+	}
+
+	//Remove dead Nodes out of write loop to avoid trashing loop index
+	for(i=0;i<cleanup.length;i+=1) {
+		clientList.splice(clientList.indexOf(cleanup[i]), 1);
+	}
+}
+
+chatServer.listen(9000);
